19905b7e3fc709a234148e251d4479021a5478b3
Paxyifier <59118083+Paxyifier@users.noreply.github.com>
2022-12-16 08:55:55 +0000

    seperate chaining working

--- src/seperate-chaining.cpp
+++ src/seperate-chaining.cpp
@@ -3,46 +3,55 @@
 
 using namespace std;
 
-struct Node {
+struct Node
+{
     int key;
-    Node* next;
+    Node *next;
 };
 
-unordered_map<int, Node*> hashTable;
+unordered_map<int, Node *> hashTable;
 
-int hashFunction(int key) {
+int hashFunction(int key)
+{
     return key % 10;
 }
 
-void insert(int key) {
+void insert(int key)
+{
     int index = hashFunction(key);
-    Node* newNode = new Node;
+    Node *newNode = new Node;
     newNode->key = key;
     newNode->next = NULL;
-    if (hashTable[index] == NULL) {
+    if (hashTable[index] == NULL)
+    {
         hashTable[index] = newNode;
-    } else {
-        Node
-temp = hashTable[index];
-while (temp->next != NULL) {
-temp = temp->next;
+    }
+    else
+    {
+        Node *temp = hashTable[index];
+        while (temp->next != NULL)
+        {
+            temp = temp->next;
+        }
+        temp->next = newNode;
+    }
 }
-temp->next = newNode;
-}
-}
-int main() {
-insert(12);
-insert(25);
-insert(36);
-insert(49);
-insert(50);
-for (int i = 0; i < 10; i++) {
-Node* temp = hashTable[i];
-while (temp != NULL) {
-cout << temp->key << " ";
-temp = temp->next;
-}
-cout << endl;
-}
-return 0;
+int main()
+{
+    insert(12);
+    insert(25);
+    insert(36);
+    insert(49);
+    insert(50);
+    for (int i = 0; i < 10; i++)
+    {
+        Node *temp = hashTable[i];
+        while (temp != NULL)
+        {
+            cout << temp->key << " ";
+            temp = temp->next;
+        }
+        cout << endl;
+    }
+    return 0;
 }
